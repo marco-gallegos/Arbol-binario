a1f0c4219f6e898b60b3adb94024e663edbac447
Marco Gallegos <ma_galeza@hotmail.com>
2016-12-07 18:37:41 -0600

    Insert and inorden definition

--- bst.h
+++ bst.h
@@ -7,7 +7,7 @@ template<class DATO> class Nodo
 {
 public:
 
-    Nodo(DATO dat = NULL, iz = NULL, de = NULL): valor(dat), izq(iz), der(de){}
+    Nodo(DATO dat,Nodo<DATO> *iz = NULL, Nodo<DATO> *de = NULL): valor(dat), izq(iz), der(de){}
     ~Nodo(){}
     DATO valor;
     //izq(izquierda  left) der(derecha right)
@@ -20,15 +20,71 @@ public:
     BinarySearchTree():root(NULL){}
     ~BinarySearchTree(){}
 
-    DATO *root;
+    Nodo<DATO> *root;
 
-    bool insert(DATO to_insert);
+    bool Empty(const Nodo<DATO> *nod);
+    bool Insert( DATO to_insert);
     bool is_in_tree(DATO to_search);
 
 
     void trim(DATO to_del);
 
+    void in_orden(void (* func)(DATO),Nodo<DATO> *nodo = NULL, bool root = true);
+
     int deepness();
 };
 
+template<class DATO>  bool BinarySearchTree<DATO>::Empty(const Nodo<DATO> *nod)
+{
+    return nod==NULL;
+}
 
+template<class DATO> bool BinarySearchTree<DATO>::Insert( DATO to_insert)
+{
+    if (Empty(this->root)) {
+        this->root = new Nodo<DATO>(to_insert);
+        return true;
+    }
+    Nodo<DATO> *it = this->root;
+
+    while(it->valor != to_insert) {
+        if(to_insert < it->valor){
+
+            if(it->izq == NULL){
+                it->izq = new Nodo<DATO>(to_insert);
+                return true;
+            }
+
+            it = it->izq;
+        }
+
+        if(to_insert > it->valor){
+
+            if(it->der == NULL){
+
+                it->der = new Nodo<DATO>(to_insert);
+                return true;
+            }
+            it = it->der;
+        }
+        if(it->valor == to_insert)
+        {
+            return false;
+        }
+    }
+
+    return false;
+}
+
+template<class DATO> void BinarySearchTree<DATO>::in_orden(void (* func)(DATO),Nodo<DATO> *nodo, bool root){
+    if(root){
+        nodo=this->root;
+    }
+    if(nodo->izq != NULL){
+        in_orden(func ,nodo->izq,false);
+    }
+    func(nodo->valor);
+    if(nodo->der != NULL){
+        in_orden(func, nodo->der,false);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include "bst.h"
 
 using namespace std;
+void show_int(int to_show)
+{
+    cout<<to_show<<" - ";
+    return;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    BinarySearchTree<int> *arbolint = new BinarySearchTree<int>();
+    int opc = 0;
+    int aux_int = 0;
+    while(opc!=9)
+    {
+        cout<<"1 - insert\t2 - delete\t3 - inorden\t9-salir"<<endl;
+        cin>>opc;cin.ignore();
+        switch (opc) {
+        case 1:
+            cout<<"int to insert  -> ";
+            cin>>aux_int;cin.ignore();
+            arbolint->Insert(aux_int);
+            break;
+        case 2:
+            break;
+        case 3:
+            arbolint->in_orden(show_int);
+            cout<<endl;
+            break;
+        default:
+            cout<<"invalido"<<endl;
+            break;
+        }
+    }
     return 0;
 }
 
